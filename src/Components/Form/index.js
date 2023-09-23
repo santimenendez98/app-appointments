30075fa8a453bb129ad26b1aedf9f9c86761c8c3
santimenendez98 <santi.mm11@gmail.com>
2023-09-23 13:55:13 -0300

    Fixed styles for select pet in form

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -408,7 +408,7 @@ function Form({ id, close, resetId, setMessageModal, showToastModal }) {
                   </label>
                   <select
                     onChange={(e) => selectedPet(e)}
-                    className="block w-26 h-10 md-sm:w-60 rounded-md border-0 py-1.5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-26 h-10 rounded-md border-0 py-1.5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   >
                     {appointmentToEdit[0].pet.map((pet) => (
                       <option>{pet.petName}</option>
